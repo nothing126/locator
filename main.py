3247beb89c7dafae31e390c0a38767e22187ca81
bogdan <ajigalcoce0168@gmail.com>
2023-09-07 20:54:08 +0300

    session changes

--- main.py
+++ main.py
@@ -1,76 +1,81 @@
 import telebot
 from telebot import types
+import datetime
+import threading
 
 # Замените 'YOUR_BOT_TOKEN' на токен вашего Telegram бота
-bot = telebot.TeleBot('6105834784:AAEYhTBRhpRFVo2oU3WW8C1VVaco5BK6YV0')
+TOKEN = '6105834784:AAEYhTBRhpRFVo2oU3WW8C1VVaco5BK6YV0'
 
-# Создаем словарь для хранения username пользователей
-user_usernames = {}
-user_locations = {}
+class MeetingBot:
+    def __init__(self, token):
+        self.bot = telebot.TeleBot(token)
+        self.sessions = {}  # Словарь для хранения сеансов по chat_id
+        self.session_timeout = 3600  # 1 час в секундах
 
+    def start(self):
+        @self.bot.message_handler(commands=['start'])
+        def start(message):
+            chat_id = message.chat.id
+            if chat_id < 0:  # Проверка, что это групповой чат
+                print(f"Bot started in group chat with id: {chat_id}")
+            self.bot.send_message(chat_id, "Здравствуйте, этот бот поможет выбрать вам и вашим друзьям место встречи исходя "
+                                      "из выбранной вами категории и вашего местоположения. Для начала перейдите в личные "
+                                      "сообщения с ботом и запустите его, далее администратор должен ввести команду '/meet' для "
+                                      "начала выбора места встречи. Далее следуйте просьбам бота")
 
-# Обработчик команды /start
-@bot.message_handler(commands=['start'])
-def start(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, "Привет! Чтобы начать, отправьте мне список username, каждый с новой строки.")
+        @self.bot.message_handler(func=lambda message: message.text.lower() == '/meet')
+        def meet(message):
+            chat_id = message.chat.id
+            if chat_id < 0:  # Проверка, что это групповой чат
+                print(f"Bot initiated /meet command in group chat with id: {chat_id}")
+            user_id = message.from_user.id
+            chat_members = self.bot.get_chat_members_count(chat_id)
+            chat_member = self.bot.get_chat_member(chat_id, user_id)
 
-    # Регистрируем следующее сообщение как обработчик для получения списка username
-    bot.register_next_step_handler(message, get_usernames)
+            if chat_members <= 2:
+                self.bot.send_message(chat_id, "Вам нужно более двух участников для создания встречи.")
+            elif chat_member.status == "administrator" or chat_member.status == "creator":
+                if chat_id not in self.sessions:
+                    self.sessions[chat_id] = {'organizer': user_id, 'participants': [], 'last_interaction': datetime.datetime.now()}
+                self.bot.send_message(chat_id, "Организатор ввел команду /meet. Ожидаю список username.")
+            else:
+                self.bot.send_message(chat_id,
+                                      "Доступ к команде /meet имеет только создатель/владелец")
 
+        @self.bot.message_handler(func=lambda message: message.text.lower() == '/stop_meet')
+        def stop_meet(message):
+            chat_id = message.chat.id
+            if chat_id in self.sessions:
+                del self.sessions[chat_id]
+                self.bot.send_message(chat_id, "Сеанс завершен.")
 
-# Функция для получения списка username
-def get_usernames(message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id
+        def check_session_timeout():
+            current_time = datetime.datetime.now()
+            sessions_to_remove = []
 
-    # Разбиваем полученный текст на строки и сохраняем в список
-    usernames_text = message.text
-    usernames_list = usernames_text.split('\n')
+            for chat_id, session in self.sessions.items():
+                last_interaction = session.get('last_interaction')
+                if last_interaction and (current_time - last_interaction).seconds >= self.session_timeout:
+                    sessions_to_remove.append(chat_id)
 
-    # Сохраняем список username в словаре с ключом в виде user_id
-    user_usernames[user_id] = usernames_list
+            for chat_id in sessions_to_remove:
+                del self.sessions[chat_id]
+                print(f"Session in group chat with id {chat_id} timed out and was removed.")
 
-    bot.send_message(chat_id, "Спасибо! Теперь отправьте мне свою геолокацию.")
-    bot.register_next_step_handler(message, get_location)
+        # Запускаем функцию проверки сессий через интервал времени (например, каждую минуту)
+        threading.Timer(60, check_session_timeout).start()
 
+        self.bot.polling()
 
-# Функция для получения местоположения
-def get_location(message):
-    user_id = message.from_user.id
-    usernames_list = user_usernames.get(user_id)
+    def get_session(self, chat_id):
+        return self.sessions.get(chat_id)
 
-    if usernames_list:
-        # Отправляем личные сообщения каждому username из списка
-        for username in usernames_list:
-            # Создаем клавиатуру с кнопкой для отправки геолокации
-            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-            item = types.KeyboardButton("Отправить геолокацию", request_location=True)
-            markup.add(item)
+    def add_participant(self, chat_id, user_id):
+        session = self.get_session(chat_id)
+        if session:
+            session['participants'].append(user_id)
+        session['last_interaction'] = datetime.datetime.now()  # Обновляем время последнего взаимодействия
 
-            # Отправляем личное сообщение с кнопкой геолокации
-            bot.send_message(user_id, f"Привет, {username}! Пожалуйста, отправьте мне свою геолокацию.", reply_markup=markup)
-    else:
-        bot.send_message(user_id, "Пожалуйста, сначала отправьте список username с помощью команды /start.")
-
-
-# Обработчик для получения местоположения
-@bot.message_handler(content_types=['location'])
-def handle_location(message):
-    user_id = message.from_user.id
-    username = user_usernames.get(user_id)  # Получаем username из словаря
-
-    if username:
-        # Формируем строку с координатами пользователя
-        location = f"{message.location.latitude}, {message.location.longitude}"
-
-        # Записываем локацию пользователя в словарь
-        user_locations[username] = location
-
-        bot.send_message(user_id, f"Спасибо! Ваше местоположение ({location}) сохранено.")
-    else:
-        bot.send_message(user_id, "Пожалуйста, сначала отправьте список username с помощью команды /start.")
-
-# Запуск бота
 if __name__ == '__main__':
-    bot.polling()
+    bot = MeetingBot(TOKEN)
+    bot.start()
